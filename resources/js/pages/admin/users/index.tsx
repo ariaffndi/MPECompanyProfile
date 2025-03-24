e841293552edf1167531b134038bc505055edce6
ari afandi <ari.affndii.com>
2025-03-24 20:25:44 +0700

    `Refactor UI components and routes for user management`

--- resources/js/pages/admin/users/index.tsx
+++ resources/js/pages/admin/users/index.tsx
@@ -22,14 +22,14 @@ export default function Users() {
       <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Users" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-               <Link href={route('register')} className="btn btn-info w-fit rounded-xl">
+               <Link href={route('register')} className="btn btn-sm btn-info w-fit rounded-xl">
                   Tambah Data
                </Link>
 
-               <div className="rounded-box border-base-content/5 bg-base-100 overflow-x-auto border">
-                  <table className="table">
+               <div className="rounded-box border-base-content/5 overflow-x-auto border">
+                  <table className="table text-center">
                         {/* head */}
-                        <thead className="bg-base-200 text-base-content text-center">
+                        <thead className="bg-base-200 text-base-content">
                            <tr>
                               <th>No</th>
                               <th>Id</th>
@@ -37,13 +37,13 @@ export default function Users() {
                               <th>Email Address</th>
                            </tr>
                         </thead>
-                        <tbody className="text-center">
+                        <tbody>
                            {users?.map((user, index) => (
-                              <tr key={user.id}>
-                                 <td>{index + 1}</td>
-                                 <th>{user.id}</th>
-                                 <td>{user.name}</td>
-                                 <td>{user.email}</td>
+                              <tr key={user.id} className="border-base-content/5 border-1">
+                                    <td>{index + 1}</td>
+                                    <th>{user.id}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
                               </tr>
                            ))}
                         </tbody>
--- routes/auth.php
+++ routes/auth.php
@@ -35,7 +35,7 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('admin/register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
