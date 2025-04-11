9af6eedd35ebdbb913ff54f4784b98744212dd49
frans <ditofrans22@gmail.com>
2025-04-11 23:20:17 +0700

    memperbaiki seeder

--- app/Models/Perusahaan.php
+++ app/Models/Perusahaan.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Perusahaan extends Model
 {
+    protected $table = 'perusahaan';
+    
     protected $fillable = [
         'nama_perusahaan',
         'alamat_perusahaan',
--- database/seeders/PerusahaanSeeder.php
+++ database/seeders/PerusahaanSeeder.php
@@ -9,16 +9,20 @@ class PerusahaanSeeder extends Seeder
 {
     public function run()
     {
-        // Perusahaan::create([
-        //     'nama_perusahaan' => 'PT Lorem Ipsum',
-        //     'alamat_perusahaan' => 'Jl. Lorem Ipsum, No. 123',
-        //     'email_perusahaan' => 'lorem@ipsum',
-        //     'no_telp_perusahaan' => '021-12345678',
-        //     'whatsapp_perusahaan' => '08123456789',
-        //     'deskripsi_perusahaan' => 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia. ',
-        //     'instagram_perusahaan' => '@loremipsum',
-        //     'facebook_perusahaan' => 'facebook.com/loremipsum',
-        //     'foto_kantor_perusahaan' => 'kantor.jpg',
-        // ]);
+        Perusahaan::create([
+            'nama_perusahaan' => 'PT. Mitra Prima Enviro',
+            'alamat_perusahaan' => 'Jl. Kertajaya Indah Timur VI No.2, Manyar Sabrangan, Kota SBY, Jawa Timur 60116',
+            'email_perusahaan' => 'admin@mitraprimaenviro.com',
+            'no_telp_perusahaan' => '031-5924526',
+            'whatsapp_perusahaan' => '+62 812-5942-9377',
+            'deskripsi_perusahaan' => 'PT Mitra Prima Enviro merupakan salah satu layanan Achmad & Assosiates Group yang bergerak dibidang Enviromental Service meliputi Layanan WWTP Design and Build, WWTP Equipment Supply, dan WWTP Operational - Maintenance Service.
+                Sejak berdiri pada 19 Maret 2009, PT Mitra Prima Enviro telah berpengalaman mengerjakan berbagai jenis Instalasi Pengolahan Air Limbah (IPAL) baik Domestik maupun Non-Domestik seperti Industri, Medis dan Laboratorium dengan jangkauan pekerjaan seluruh Indonesia.
+                Dengan beberapa pengalaman proyek baik dalam instansi pemerintah maupun swasta, PT Mitra Prima Enviro senantiasa berkomitmen untuk dapat berkonstribusi dalam permasalahan lingkungan dan selalu meningkatkan kinerja dan jasa layanan, baik melalui profesionalisme kerja maupun inovatif dalam setiap kemajuan teknologi di bidang Teknik Lingkungan',
+            'instagram_perusahaan' => '@mitraprimaenviro',
+            'facebook_perusahaan' => 'facebook.com/mitraenviro',
+            'foto_kantor_perusahaan' => 'kantor.jpg',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
