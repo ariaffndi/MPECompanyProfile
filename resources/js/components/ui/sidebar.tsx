d27b1954f7fb7daa90a33632e891e0ed80e231d4
ari afandi <ari.affndii.com>
2025-04-15 21:57:07 +0700

    memperbaiki responsif tabel

--- resources/js/components/ui/sidebar.tsx
+++ resources/js/components/ui/sidebar.tsx
@@ -304,7 +304,7 @@ function SidebarInset({ className, ...props }: React.ComponentProps<"main">) {
     <main
       data-slot="sidebar-inset"
       className={cn(
-        "mx-auto flex h-full w-full max-w-7xl flex-1 flex-col gap-4 rounded-xl bg-background min-h-svh",
+        "mx-auto flex h-full w-full flex-1 flex-col gap-4 rounded-xl bg-background min-h-svh",
         // "bg-background relative flex min-h-svh flex-1 flex-col",
         "peer-data-[variant=inset]:min-h-[calc(100svh-(--spacing(4)))] md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-0",
         className
