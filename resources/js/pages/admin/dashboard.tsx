9673c671b1b4d0ab539797d6c58ceca6ed1583a2
ari afandi <ari.affndii.com>
2025-04-17 23:15:01 +0700

    memperbaiki chart dashboard

--- resources/js/pages/admin/dashboard.tsx
+++ resources/js/pages/admin/dashboard.tsx
@@ -41,10 +41,7 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-5">
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
+                <div className="grid auto-rows-min grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
@@ -58,62 +55,60 @@ export default function Dashboard() {
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
                 </div>
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>Area Chart - Stacked</CardTitle>
-                            <CardDescription>Showing total visitors for the last 6 months</CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            <ChartContainer config={chartConfig} className='h-[200px] w-full'>
-                                <AreaChart
-                                    accessibilityLayer
-                                    data={chartData}
-                                    margin={{
-                                        left: 12,
-                                        right: 12,
-                                    }}
-                                >
-                                    <CartesianGrid vertical={false} />
-                                    <XAxis
-                                        dataKey="month"
-                                        tickLine={false}
-                                        axisLine={false}
-                                        tickMargin={8}
-                                        tickFormatter={(value) => value.slice(0, 3)}
-                                    />
-                                    <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="dot" />} />
-                                    <Area
-                                        dataKey="mobile"
-                                        type="natural"
-                                        fill="var(--color-mobile)"
-                                        fillOpacity={0.4}
-                                        stroke="var(--color-mobile)"
-                                        stackId="a"
-                                    />
-                                    <Area
-                                        dataKey="desktop"
-                                        type="natural"
-                                        fill="var(--color-desktop)"
-                                        fillOpacity={0.4}
-                                        stroke="var(--color-desktop)"
-                                        stackId="a"
-                                    />
-                                </AreaChart>
-                            </ChartContainer>
-                        </CardContent>
-                        <CardFooter>
-                            <div className="flex w-full items-start gap-2 text-sm">
-                                <div className="grid gap-2">
-                                    <div className="flex items-center gap-2 leading-none font-medium">
-                                        Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                                    </div>
-                                    <div className="text-muted-foreground flex items-center gap-2 leading-none">January - June 2024</div>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Area Chart - Stacked</CardTitle>
+                        <CardDescription>Showing total visitors for the last 6 months</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <ChartContainer config={chartConfig} className="h-[280px] w-full">
+                            <AreaChart
+                                accessibilityLayer
+                                data={chartData}
+                                margin={{
+                                    left: 12,
+                                    right: 12,
+                                }}
+                            >
+                                <CartesianGrid vertical={false} />
+                                <XAxis
+                                    dataKey="month"
+                                    tickLine={false}
+                                    axisLine={false}
+                                    tickMargin={8}
+                                    tickFormatter={(value) => value.slice(0, 3)}
+                                />
+                                <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="dot" />} />
+                                <Area
+                                    dataKey="mobile"
+                                    type="natural"
+                                    fill="var(--color-mobile)"
+                                    fillOpacity={0.4}
+                                    stroke="var(--color-mobile)"
+                                    stackId="a"
+                                />
+                                <Area
+                                    dataKey="desktop"
+                                    type="natural"
+                                    fill="var(--color-desktop)"
+                                    fillOpacity={0.4}
+                                    stroke="var(--color-desktop)"
+                                    stackId="a"
+                                />
+                            </AreaChart>
+                        </ChartContainer>
+                    </CardContent>
+                    <CardFooter>
+                        <div className="flex w-full items-start gap-2 text-sm">
+                            <div className="grid gap-2">
+                                <div className="flex items-center gap-2 leading-none font-medium">
+                                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
                                 </div>
+                                <div className="text-muted-foreground flex items-center gap-2 leading-none">January - June 2024</div>
                             </div>
-                        </CardFooter>
-                    </Card>
-                </div>
+                        </div>
+                    </CardFooter>
+                </Card>
             </div>
         </AppLayout>
     );
