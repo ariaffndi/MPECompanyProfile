cf7235a75c092dcd28a34206ca88c869cff53d2a
ari <ari.affndii@gmail.com>
2025-04-17 17:22:05 +0700

    menambahkan chart component pada dashboard

--- resources/js/pages/admin/dashboard.tsx
+++ resources/js/pages/admin/dashboard.tsx
@@ -1,7 +1,12 @@
+'use client';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { TrendingUp } from 'lucide-react';
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -11,11 +16,38 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Dashboard() {
+
+    const chartData = [
+        { month: 'January', desktop: 186, mobile: 80 },
+        { month: 'February', desktop: 305, mobile: 200 },
+        { month: 'March', desktop: 237, mobile: 120 },
+        { month: 'April', desktop: 73, mobile: 190 },
+        { month: 'May', desktop: 209, mobile: 130 },
+        { month: 'June', desktop: 214, mobile: 140 },
+    ];
+    const chartConfig = {
+        desktop: {
+            label: 'Desktop',
+            color: 'hsl(var(--chart-1))',
+        },
+        mobile: {
+            label: 'Mobile',
+            color: 'hsl(var(--chart-2))',
+        },
+    } satisfies ChartConfig;
+
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="grid auto-rows-min gap-4 md:grid-cols-5">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
@@ -27,7 +59,60 @@ export default function Dashboard() {
                     </div>
                 </div>
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Area Chart - Stacked</CardTitle>
+                            <CardDescription>Showing total visitors for the last 6 months</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <ChartContainer config={chartConfig} className='h-[200px] w-full'>
+                                <AreaChart
+                                    accessibilityLayer
+                                    data={chartData}
+                                    margin={{
+                                        left: 12,
+                                        right: 12,
+                                    }}
+                                >
+                                    <CartesianGrid vertical={false} />
+                                    <XAxis
+                                        dataKey="month"
+                                        tickLine={false}
+                                        axisLine={false}
+                                        tickMargin={8}
+                                        tickFormatter={(value) => value.slice(0, 3)}
+                                    />
+                                    <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="dot" />} />
+                                    <Area
+                                        dataKey="mobile"
+                                        type="natural"
+                                        fill="var(--color-mobile)"
+                                        fillOpacity={0.4}
+                                        stroke="var(--color-mobile)"
+                                        stackId="a"
+                                    />
+                                    <Area
+                                        dataKey="desktop"
+                                        type="natural"
+                                        fill="var(--color-desktop)"
+                                        fillOpacity={0.4}
+                                        stroke="var(--color-desktop)"
+                                        stackId="a"
+                                    />
+                                </AreaChart>
+                            </ChartContainer>
+                        </CardContent>
+                        <CardFooter>
+                            <div className="flex w-full items-start gap-2 text-sm">
+                                <div className="grid gap-2">
+                                    <div className="flex items-center gap-2 leading-none font-medium">
+                                        Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+                                    </div>
+                                    <div className="text-muted-foreground flex items-center gap-2 leading-none">January - June 2024</div>
+                                </div>
+                            </div>
+                        </CardFooter>
+                    </Card>
                 </div>
             </div>
         </AppLayout>
