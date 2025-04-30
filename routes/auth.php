c88594cfbbe5c66ab593e47f72ca4dbdad56f050
ari afandi <ari.affndii.com>
2025-04-30 23:38:02 +0700

    memperbaiki route register

--- routes/auth.php
+++ routes/auth.php
@@ -35,8 +35,8 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::get('admin/register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('admin/register', [RegisteredUserController::class, 'store']);
     
     Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
