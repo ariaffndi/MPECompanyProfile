fc5e4c798f03ea48f665d416fc133f6542d4b794
frans <ditofrans22@gmail.com>
2025-05-03 21:51:16 +0700

    membuat layout

--- resources/js/layouts/guestLayout.tsx
+++ resources/js/layouts/guestLayout.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from '../components/mainApp/navbar';
 
-export default function GuestLayout({ children }) {
+type Props  = {
+    children : React.ReactNode
+}
+
+const GuestLayout : React.FunctionComponent<Props> = ({children}) => {
     return (
         <div>
             <Navbar />
             <main>{children}</main>
-            {/* <Footer /> */}
         </div>
     );
 }
+
+export default GuestLayout;
