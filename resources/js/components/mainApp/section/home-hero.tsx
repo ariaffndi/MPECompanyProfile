db9505d2cf7de7806074c2fd82882e9f2509399d
frans <ditofrans22@gmail.com>
2025-05-05 01:30:30 +0700

    membuat halaman section

--- resources/js/components/mainApp/section/home-hero.tsx
+++ resources/js/components/mainApp/section/home-hero.tsx
@@ -2,15 +2,14 @@ import bgHome from '@/assets/images/bg-home.jpg';
 
 const HomeHero = () => {
     return (
-        <div className="hero w-full min-h-screen    bg-cover bg-center" style={{ backgroundImage: `url(${bgHome})` }}>
-
-            <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="hero min-h-screen w-full bg-cover bg-center" style={{ backgroundImage: `url(${bgHome})` }}>
+            <div className="absolute inset-0 bg-black opacity-70"></div>
             <div className="hero-content flex-col text-white lg:flex-row-reverse">
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <h1 className="text-5xl font-bold">Mitra Prima Enviro .</h1>
                     <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                        repudiandae et a id nisi.
+                        Merupakan salah satu layanan Achmad & Assosiates Group yang bergerak dibidang Enviromental Service meliputi Layanan WWTP
+                        Design and Build,<br /> WWTP Equipment Supply, dan WWTP Operational - Maintenance Service.
                     </p>
                     <button className="btn rounded-2xl bg-blue-400 text-white">Discover More</button>
                 </div>
