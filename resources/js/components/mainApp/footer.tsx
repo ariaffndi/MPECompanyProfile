bdfca5874209f8cfd648c312737adeaab5ccbcd3
frans <ditofrans22@gmail.com>
2025-05-04 18:56:24 +0700

    menanambahkan data dari db ke footer

--- resources/js/components/mainApp/footer.tsx
+++ resources/js/components/mainApp/footer.tsx
@@ -1,31 +1,52 @@
-import { Facebook, Instagram, Mail, Phone } from 'lucide-react';
+import Company from '@/pages/admin/company';
+import { faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { usePage } from '@inertiajs/react';
+import { Facebook, Mail, Phone } from 'lucide-react';
+
 import AppLogoIcon from '../app-logo-icon';
 
-const footer: React.FC = () => {
+type Company = {
+    name: string;
+    address: string;
+    email: string;
+    phone: string;
+    whatsapp: string;
+    instagram: string;
+    facebook: string;
+};
+
+const footer = () => {
+    const { company } = usePage<{ company: Company }>().props;
     const currentYear: number = new Date().getFullYear();
 
     return (
         <div>
             <footer className="footer sm:footer-horizontal justify-between bg-sky-900 p-10 text-white">
-                <aside className="flex flex-col items-center md:items-start text-center md:text-left gap-4">
-                    <AppLogoIcon className="w-30 md:w-28 mb-0 md:mb-12 aspect-square bg-white p-2 rounded-lg shadow-md" />
+                <aside className="flex flex-col items-center gap-4 text-center md:items-start md:text-left">
+                    <AppLogoIcon className="mb-0 aspect-square w-30 rounded-lg bg-white p-2 shadow-md md:mb-12 md:w-28" />
                     <div className="text-sm leading-snug">
-                        <p>Copyright © {new Date().getFullYear()} PT. Mitra Prima Enviro</p>
-                        <p>Designed and Developed by | PT. Mitra Prima Enviro. | All rights reserved</p>
+                        <p>
+                            Copyright © {currentYear} {company.name} .
+                        </p>
+                        <p>Designed and Developed by | {company.name} . | All rights reserved</p>
                     </div>
                 </aside>
 
-                <div className="flex flex-col gap-6 text-sm items-center  md:items-end">
+                <div className="flex flex-col items-center gap-6 text-sm md:items-end">
                     <div className="flex flex-col items-end gap-2">
                         <div className="flex gap-3">
-                            <a className="link" href="#">
-                                <Instagram />
+                            <a className="link" href={company.instagram} target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faInstagram} className="text-2xl" />
                             </a>
-                            <a className="link" href="#">
+                            <a className="link" href={company.facebook} target="_blank" rel="noopener noreferrer">
                                 <Facebook />
                             </a>
-                            <a className="link" href="#">
+                            <a className="link" href={`mailto:${company.email}`} target="_blank" rel="noopener noreferrer">
                                 <Mail />
+                            </a>
+                            <a className="link" href={`https://wa.me/${company.whatsapp}`} target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faWhatsapp} className='text-2xl'/>
                             </a>
                         </div>
                         <nav className="flex flex-col text-center md:text-right">
@@ -36,19 +57,19 @@ const footer: React.FC = () => {
                         </nav>
                     </div>
 
-                    <div className="text-center md:text-right leading-snug">
-                        <p className="flex items-center justify-center md:justify-end gap-2">
+                    <div className="text-center leading-snug md:text-right">
+                        <p className="flex items-center justify-center gap-2 md:justify-end">
                             <Phone />
-                            <a href="tel:0315924528" >
-                                (031) 5924528
-                            </a>
+                            <a href={`tel:${company.phone}`}>{company.phone}</a>
                         </p>
                         <p>
-                            Jl. Kertajaya Indah Timur VI No.2, Manyar
-                            <br />
-                            Sabrangan, Kec. Mulyorejo, Kota SBY,
-                            <br />
-                            Jawa Timur 60116
+                            {company.address &&
+                                company.address.split(',').map((line, index) => (
+                                    <span key={index}>
+                                        {line}
+                                        <br />
+                                    </span>
+                                ))}
                         </p>
                     </div>
                 </div>
