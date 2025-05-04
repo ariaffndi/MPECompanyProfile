d0e1de20e11490421d902b1e3a49a0857310b9fb
frans <ditofrans22@gmail.com>
2025-05-05 01:45:24 +0700

    membuat halaman section

--- resources/js/components/mainApp/navbar.tsx
+++ resources/js/components/mainApp/navbar.tsx
@@ -50,7 +50,7 @@ const Navbar = () => {
             </div>
 
             {/* Mobile Menu */}
-            <div className="dropdown dropdown-end ml-2 lg:hidden">
+            <div className={`dropdown dropdown-end ml-2 lg:hidden ${isScrolled ? "text-base-content" : "text-white"}`}>
                <div tabIndex={0} role="button" className="btn btn-ghost">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
--- resources/js/components/mainApp/section/home-hero.tsx
+++ resources/js/components/mainApp/section/home-hero.tsx
@@ -6,7 +6,7 @@ const HomeHero = () => {
             <div className="absolute inset-0 bg-black opacity-70"></div>
             <div className="hero-content flex-col text-white lg:flex-row-reverse">
                 <div>
-                    <h1 className="text-5xl font-bold">Mitra Prima Enviro .</h1>
+                    <h1 className="text-5xl font-bold border-b-4 m-2 w-1/2 pb-4">Mitra Prima Enviro .</h1>
                     <p className="py-6">
                         Merupakan salah satu layanan Achmad & Assosiates Group yang bergerak dibidang Enviromental Service meliputi Layanan WWTP
                         Design and Build,<br /> WWTP Equipment Supply, dan WWTP Operational - Maintenance Service.
