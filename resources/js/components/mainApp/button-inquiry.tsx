6bb2bea3c0772f9a2aebce476b254543665b325c
frans <ditofrans22@gmail.com>
2025-05-05 16:04:27 +0700

    membuat scrollbar

--- resources/js/components/mainApp/button-inquiry.tsx
+++ resources/js/components/mainApp/button-inquiry.tsx
@@ -4,7 +4,7 @@ import { ShoppingCart } from 'lucide-react';
 
 export default function ButtonInquiry() {
   return (
-      <Link href='#' className={'btn btn-sm bg-blue-400 w-fit rounded-lg text-base-200'}>
+      <Link href='#' className={'btn btn-sm border-0 shadow-lg bg-blue-400 w-fit rounded-lg text-white'}>
           <ShoppingCart size={16} /> Pemesanan IPAL
       </Link>
   );
