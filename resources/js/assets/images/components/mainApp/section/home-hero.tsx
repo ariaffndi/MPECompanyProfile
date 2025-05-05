24ee4dc34de9f192d61e7afbe68c582aa003f254
frans <ditofrans22@gmail.com>
2025-05-05 15:57:34 +0700

    membuat scrollbar

--- resources/js/assets/images/components/mainApp/section/home-hero.tsx
+++ resources/js/assets/images/components/mainApp/section/home-hero.tsx
@@ -13,7 +13,7 @@ const HomeHero = () => {
             <div className="absolute inset-0 bg-black opacity-70"></div>
             <div className="hero-content flex-col text-white lg:flex-row-reverse">
                 <div>
-                    <h1 className="m-2 w-full border-b-4 pb-4 text-5xl font-bold md:w-1/2">{company.name} .</h1>
+                    <h1 className="m-2 w-full border-b-4 pb-4 text-5xl font-bold md:w-1/2">{company.name}</h1>
                     <p className="w-full py-6 md:w-1/2">{company.description.split(/\.\s*/)[0].trim()}.</p>
                     <button className="btn rounded-2xl bg-blue-400 text-white">Discover More</button>
                 </div>
